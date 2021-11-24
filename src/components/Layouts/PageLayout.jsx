d71f4707f3c9ac194d5f7eca0afb15c4911f6d02
Nattapong Saengphrom <wazzzeez@gmail.com>
2021-11-24 22:46:51 +0700

    fix `ExplorerPage` lazy load

--- src/components/Layouts/PageLayout.jsx
+++ src/components/Layouts/PageLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Footer } from 'components/Footer';
 import Header from 'components/header';
+import cx from 'classnames';
 
 export function PageLayout(props) {
-  const { children, cover } = props;
+  const { className, children, cover, ...rest } = props;
   return (
-    <div className="overflow-hidden">
+    <div className={cx('overflow-hidden', className)} {...rest}>
       <Header />
       {cover}
       <div className="container">{children}</div>
