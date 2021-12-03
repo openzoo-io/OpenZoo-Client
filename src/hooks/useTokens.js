0860a9b76b14d7e72681b02b61277079047b9ebe
lolieatapple <lolieatapple@gmail.com>
2021-12-03 23:19:14 +0800

    update remove tokens

--- src/hooks/useTokens.js
+++ src/hooks/useTokens.js
@@ -55,32 +55,11 @@ const Tokens = {
       icon: iconZOO,
     },
     {
-      address: '0x830053DABd78b4ef0aB0FeC936f8a1135B68da6f',
-      name: 'WASP',
-      symbol: 'WASP',
-      decimals: 18,
-      icon: iconWASP,
-    },
-    {
       address: '0x3D5950287b45F361774E5fB6e50d70eEA06Bc167',
       name: 'wanUSDT',
       symbol: 'wanUSDT',
       decimals: 6,
       icon: iconUSDT,
-    },
-    {
-      address: '0x7fF465746e4F47e1CbBb80c864CD7DE9F13337fE',
-      name: 'wanUSDC',
-      symbol: 'wanUSDC',
-      decimals: 6,
-      icon: iconUSDC,
-    },
-    {
-      address: '0x48344649B9611a891987b2Db33fAada3AC1d05eC',
-      name: 'wanETH',
-      symbol: 'wanETH',
-      decimals: 18,
-      icon: iconETH,
     },
   ],
 };
