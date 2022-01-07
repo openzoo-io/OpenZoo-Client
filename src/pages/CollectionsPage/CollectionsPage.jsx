cb3eb04c1768b91f904d5086000c37cca6c1d32a
moohooooo <suwichalala@gmail.com>
2022-01-07 15:20:25 +0700

    Update CollectionsPage.jsx

--- src/pages/CollectionsPage/CollectionsPage.jsx
+++ src/pages/CollectionsPage/CollectionsPage.jsx
@@ -4,26 +4,29 @@ import { CollectionsPageFilterStatus } from './components/CollectionsPageFilterS
 import { NFTCollection } from 'components/NFTCollection';
 import { useSelector } from 'react-redux';
 import { useApi } from 'api';
-
+import Loader from 'react-loader-spinner';
 export function CollectionsPage() {
   const { fetchCollectionList } = useApi();
   const [collections, setCollections] = useState([]);
   const [sortedBy, setSortedBy] = useState({id:''});
-  
+  const [loading, setLoading] = useState(false);
   const {
     onlyVerified,
   } = useSelector(state => state.Filter);
 
   useEffect(() => {
+    setCollections([]);
     getAllCollections();
   }, [onlyVerified]);
 
 
 
   const getAllCollections = async () => {
+    setLoading(true);
     const res = await fetchCollectionList(onlyVerified);
     if (res.status === 'success') {
-      setCollections( res.data);
+      setCollections(res.data);
+      setLoading(false);
     }
   };
 
@@ -32,6 +35,13 @@ export function CollectionsPage() {
       <div className="section mt-50 collectionList">
         {false && <div className="section__head">{<CollectionsPageFilterStatus sortedBy={sortedBy} setSortedBy={setSortedBy}/>}</div>}
         <div className="row justify-content-center mb-30_reset">
+          {
+            loading && (
+              <div className={'d-flex justify-content-center'} >
+              <Loader type="Oval" color="#00A59A" height={40} width={40} />
+              </div>
+            )
+          }
           {collections.map((item, index) => (
             <div
               key={`collection-item-${index}`}
