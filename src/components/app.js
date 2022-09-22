6aa09f71a5d59ea7c8b755d4d8ec6da65648d2f7
moohooooo <suwichalala@gmail.com>
2022-09-23 02:00:37 +0700

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -40,6 +40,7 @@ const App = () => {
 
   useEffect(() => {
 
+    alert('OpenZoo is on maintenance mode. Please do not use it for now. We will be back soon. Thank you for your patience.');
     const fetch = () => {
       axios
         .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=zookeeper&order=market_cap_desc&per_page=100&page=1&sparkline=false')
