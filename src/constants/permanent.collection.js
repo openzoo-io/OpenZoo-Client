c530c81192656a9b03be367acff151d4fcd4812c
moohooooo <suwichalala@gmail.com>
2022-09-26 23:31:31 +0700

    add SAS as permanent nfts

--- src/constants/permanent.collection.js
+++ src/constants/permanent.collection.js
@@ -2,5 +2,5 @@ export default {
     // Collection and Seller address //
 
     // SAS => SAS owner //
-    //"0xc2b3af0a56387d4ef095a80a174f493e9a0438a5" : "0xc3760850e88b30fff7cc9177ef3bec6d160db9b1"
+    "0xc2b3af0a56387d4ef095a80a174f493e9a0438a5" : "0xc3760850e88b30fff7cc9177ef3bec6d160db9b1"
 }
